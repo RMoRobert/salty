46171cbc78a19a0534348917440152840acda934
Robert Morris <devmail@rmemail.net>
2025-11-01 13:54:59 -0500

    Add Get Info on other platforms

--- Salty/Views/RecipeNavigationSplitView.swift
+++ Salty/Views/RecipeNavigationSplitView.swift
@@ -1,3 +1,7 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU Lesser General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+
 //
 //  RecipeNavigationSplitView.swift
 //  Salty
@@ -516,12 +520,10 @@ struct RecipeNavigationSplitView: View {
             Text("Delete")
         }
         .keyboardShortcut(.delete, modifiers: [.command])
-        #if os(macOS)
         Divider()
         Button("Get Info") {
             recipeIDForInspector = recipe.id
         }
-        #endif
     }
 }
 
